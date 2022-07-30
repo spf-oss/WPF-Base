9fafa38446f1b1a9a25da968820d8ed1b5d962f1
spf <1771245717@qq.com>
2022-07-30 23:06:36 +0800

    内容第一行

--- 自动化生产管理平台/ViewModels/MainViewModel.cs
+++ 自动化生产管理平台/ViewModels/MainViewModel.cs
@@ -9,15 +9,22 @@ namespace 自动化生产管理平台
     internal class MainViewModel : NotifyPropertyChanged
     {
         private object? _pageContent;
+        private string _total = String.Empty;
+        private string _productionCount = String.Empty;
+        private string _badCount = String.Empty;
 
         public object? PageContent { get => _pageContent; set => SetProperty(ref _pageContent, value); }
-
-        private string _total = String.Empty;
         public string Total { get => _total; set => SetProperty(ref _total, value); }
+        public string ProductionCount { get => _productionCount; set => SetProperty(ref _productionCount, value); }
+        public string BadCount { get => _badCount; set => SetProperty(ref _badCount, value); }
 
         public MainViewModel()
         {
             this.Total = 197.ToString("0000");
+
+            this.BadCount = 111.ToString("000");
+
+            this.ProductionCount = 1998.ToString("0000");
         }
     }
 }
