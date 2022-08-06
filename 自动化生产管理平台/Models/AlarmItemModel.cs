53d000a009f69402e1009edf704865480fa7166a
spf <1771245717@qq.com>
2022-08-06 14:28:01 +0800

    气象图表

---
+++ 自动化生产管理平台/Models/AlarmItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 自动化生产管理平台
+{
+    internal class AlarmItemModel
+    {
+        public string Num { get; set; } = String.Empty;
+
+        public string Message { get; set; } = String.Empty;
+
+        public string Time { get; set; } = String.Empty;
+
+        public int Len { get; set; }
+    }
+}
--- 自动化生产管理平台/ViewModels/MainViewModel.cs
+++ 自动化生产管理平台/ViewModels/MainViewModel.cs
@@ -14,6 +14,8 @@ namespace 自动化生产管理平台
         private string _badCount = String.Empty;
         public List<MonitorItemModel> Evironment { get; set; }
 
+        public List<AlarmItemModel> AlarmItemModels { get; set; }
+
         public object? PageContent { get => _pageContent; set => SetProperty(ref _pageContent, value); }
         public string Total { get => _total; set => SetProperty(ref _total, value); }
         public string ProductionCount { get => _productionCount; set => SetProperty(ref _productionCount, value); }
@@ -27,7 +29,8 @@ namespace 自动化生产管理平台
 
             this.ProductionCount = 1998.ToString("0000");
 
-            this.Evironment = new List<MonitorItemModel>();
+            #region 初始化环境集合
+            this.Evironment = new();
             this.Evironment.Add(new MonitorItemModel() { Header = "光照（Lux）" });
             this.Evironment.Add(new MonitorItemModel() { Header = "噪音（db）" });
             this.Evironment.Add(new MonitorItemModel() { Header = "温度（℃）" });
@@ -35,6 +38,35 @@ namespace 自动化生产管理平台
             this.Evironment.Add(new MonitorItemModel() { Header = "PM2.5" });
             this.Evironment.Add(new MonitorItemModel() { Header = "硫化氢（ppm）" });
             this.Evironment.Add(new MonitorItemModel() { Header = "氮气（ppm）" });
+            #endregion
+
+            #region 初始化报警记录集合
+            this.AlarmItemModels = new();
+            this.AlarmItemModels.Add(
+                new AlarmItemModel()
+                {
+                    Len = 1,
+                    Message = "我现在好烦",
+                    Num = "2",
+                    Time = DateTime.Now.ToString("yyyy/MM/dd")
+                });
+            this.AlarmItemModels.Add(
+                new AlarmItemModel()
+                {
+                    Len = 1,
+                    Message = "我现在好烦",
+                    Num = "2",
+                    Time = DateTime.Now.ToString("yyyy/MM/dd")
+                });
+            this.AlarmItemModels.Add(
+                new AlarmItemModel()
+                {
+                    Len = 1,
+                    Message = "我现在好烦",
+                    Num = "2",
+                    Time = DateTime.Now.ToString("yyyy/MM/dd")
+                }); 
+            #endregion
         }
     }
 }
